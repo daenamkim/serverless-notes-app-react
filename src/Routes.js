7c5a5e2e92c2d630130e74b85eacf6f1b07c0008
Daenam Kim <daenam.kim@gmail.com>
2018-04-12 19:29:08 +0900

    Add signup codes.

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Signup from './containers/Signup';
 import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
 import AppliedRoute from "./components/AppliedRoute";
@@ -8,6 +9,7 @@ import AppliedRoute from "./components/AppliedRoute";
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
+    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <Route component={NotFound} />
   </Switch>;
