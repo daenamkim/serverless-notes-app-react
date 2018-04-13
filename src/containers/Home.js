80c8b5ec6e63254fdde35fb2fe15d8f4c4cbaf2f
Daenam Kim <daenam.kim@gmail.com>
2018-04-13 14:50:07 +0900

    Connect to AWS for a note list.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { PageHeader, ListGroup } from "react-bootstrap";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./Home.css";
+import { API } from "aws-amplify";
 
 export default class Home extends Component {
   constructor(props) {
@@ -12,8 +13,55 @@ export default class Home extends Component {
     };
   }
 
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+
+    try {
+      const notes = await this.notes();
+      this.setState({notes});
+    } catch (e) {
+      alert(e);
+    }
+
+    this.setState({isLoading: false});
+  }
+
+  notes() {
+    console.log('test!!');
+    var test = API.get('notes', '/notes');
+    console.log(test);
+    return test
+  }
+
   renderNotesList(notes) {
-    return null;
+    return [{}].concat(notes).map(
+      (note, i) =>
+        i !== 0
+          ? <ListGroupItem
+              key={note.noteId}
+              href={`/notes/${note.noteId}`}
+              onClick={this.handleNoteClick}
+              header={note.content.trim().split('\n')[0]}
+            >
+              {"Created: " + new Date(note.createdAt).toLocaleString()}
+            </ListGroupItem>
+          : <ListGroupItem
+              key="new"
+              href="/notes/new"
+              onClick={this.handleNoteClick}
+            >
+              <h4>
+                <b>{'\uFF0B'}</b> Create a new note
+              </h4>
+            </ListGroupItem>
+    );
+  }
+
+  handleNoteClick = event => {
+    event.preventDefault();
+    this.props.history.push(event.currentTarget.getAttribute('href'));
   }
 
   renderLander() {
